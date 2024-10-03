dbcd90844d421e5a09fba9315132436addf8dc20
arun-kumar-0308 <arunkumart03082002@gmail.com>
2024-10-03 10:57:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const bar = document.getElementById('bar');
+const close1 = document.getElementById('close1')
+const nav = document.getElementById('navbar');
+
+if (bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+
+    })
+}
+
+if (close1){
+    close1.addEventListener('click',()=>{
+        nav.classList.remove('active');
+
+    })
+}
